20082cf009d28e78bcd5cf5bb4ba87145432fcaf
Otaviano Silvério de Sousa <otaviano@sil.inf.br>
2022-10-14 20:16:40 -0300

    Add files via upload

---
+++ PSCCMA/Lista63_Matriz.java
@@ -0,0 +1,49 @@
+package PSCCMA;
+
+import java.util.Scanner;
+
+public class Lista63_Matriz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tamanho=0;
+		 Scanner ler = new 
+		 Scanner(System.in);
+		 
+		 System.out.println("Informe o Tamanho da Matriz a ser criada ");
+	     tamanho = ler.nextInt();
+	     int [][]MatrizNova= new int[tamanho][tamanho];
+	     int Maior = 0;
+	     for(int i=0;i<tamanho;i++){
+			  System.out.println("Linha =>: "+i);
+	            for(int j=0;j<tamanho;j++){
+	                MatrizNova[i][j]= (int)(Math.random()*100)+1;
+	                // maior valor da matriz
+	                if (MatrizNova[i][j]>Maior) {
+	                	Maior = MatrizNova[i][j];
+	                }
+	                System.out.println("Coluna =>: "+MatrizNova[i][j]);
+	            }
+	        }
+	        System.out.println("Maior Numero da Matriz =>: "+Maior);
+	     
+		 
+		 
+		 
+		 
+		
+		  int [][]Matriz= new int[3][10];
+		  for(int i=0;i<3;i++){
+			  System.out.println("Linha =>: "+i);
+	            for(int j=0;j<10;j++){
+	                Matriz[i][j]=j;
+	                System.out.println("Coluna =>: "+j);
+	            }
+	        }
+		
+		  
+		  
+		  
+	}
+
+}
